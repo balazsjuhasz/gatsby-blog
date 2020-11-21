ac2f5ed134090b778adea8f18fc5b644d3ae21b3
Balazs Juhasz <juhaszbal@gmail.com>
2020-11-21 09:37:45 +0100

    finish styling blog

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,19 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import styled from "styled-components"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
 
 export default ({ data }) => {
   return (
@@ -14,9 +24,13 @@ export default ({ data }) => {
         <h4>{data.allMarkdownRemark.totalCount}</h4>
         {data.allMarkdownRemark.edges.map(({ node }) => (
           <div key={node.id}>
-            <span>
-              {node.frontmatter.title} - {node.frontmatter.date}
-            </span>
+            <BlogLink to={node.fields.slug}>
+              <BlogTitle>
+                <span>
+                  {node.frontmatter.title} - {node.frontmatter.date}
+                </span>
+              </BlogTitle>
+            </BlogLink>
             <p>{node.excerpt}</p>
           </div>
         ))}
@@ -27,7 +41,7 @@ export default ({ data }) => {
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       totalCount
       edges {
         node {
@@ -36,6 +50,9 @@ export const query = graphql`
             date
             description
             title
+          }
+          fields {
+            slug
           }
           excerpt
         }
