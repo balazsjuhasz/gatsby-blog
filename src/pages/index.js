e4138963e6c7d7a42b3eb6c61ec1a5785920e5b1
Balazs Juhasz <juhaszbal@gmail.com>
2020-11-21 09:38:34 +0100

    remove not used component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import { graphql, Link } from "gatsby"
 import styled from "styled-components"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+//import Image from "../components/image"
 import SEO from "../components/seo"
 
 const BlogLink = styled(Link)`
